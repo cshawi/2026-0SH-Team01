4cdeaee4d18e416f481c3a32fa2dd44df9758920
S-Beaulieu <Beaulieu-Salomon@protonmail.com>
2026-03-09 08:05:42 -0400

    godot struct ready to merge

---
+++ h-t-g/scripts/moveableObjects/full_motion_object.gd
@@ -0,0 +1,8 @@
+extends MoveableObject
+class_name FullMotionObject
+
+
+
+func action():
+	#aller chercher une target en x et en y 
+	pass
--- h-t-g/scripts/res/objects/base_object.gd
+++
@@ -1,7 +0,0 @@
-extends Resource;
-class_name BaseObject
-
-@export_group("Information")
-@export var name: String;
-@export var sprite: Texture2D;
-@export var mass: float;
