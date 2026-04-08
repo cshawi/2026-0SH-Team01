51531903c4b92f44ebb28e8973b12f420f4f8eab
Parzival2066931 <tomy1421630@outlook.com>
2026-04-08 16:32:57 -0400

    added floatable zone to snow_level

--- h-t-g/scripts/game_manager.gd
+++ h-t-g/scripts/game_manager.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name GameManager
 
 
-var mouse_mode := false #temporaire le temps d'avoir les paramètres
+var mouse_mode := true #temporaire le temps d'avoir les paramètres
 var player_hp: float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
